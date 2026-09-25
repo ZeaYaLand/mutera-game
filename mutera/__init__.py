e359e1e01abc2a2d91a86f1174ed99936857dc55
ZeaYaLand <danilborisov726@gmail.com>
2026-09-25 18:05:45 +0700

    feat: initialize Mutera package

---
+++ mutera/__init__.py
@@ -0,0 +1,5 @@
+"""Mutera evolutionary simulation package."""
+
+from .core import Environment, Genome, Organism, Population, World
+
+__all__ = ["Environment", "Genome", "Organism", "Population", "World"]
