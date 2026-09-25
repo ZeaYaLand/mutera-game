aeff0160d6ef7fc40d5e1e2ecd04a38bf18f55fd
ZeaYaLand <danilborisov726@gmail.com>
2026-09-25 19:28:33 +0700

    Export Stage 1 simulation API

--- mutera/__init__.py
+++ mutera/__init__.py
@@ -1,5 +1,15 @@
 """Mutera evolutionary simulation package."""
 
 from .core import Environment, Genome, Organism, Population, World
+from .stage1 import PopulationStats, Stage1Config, Stage1Simulation
 
-__all__ = ["Environment", "Genome", "Organism", "Population", "World"]
+__all__ = [
+    "Environment",
+    "Genome",
+    "Organism",
+    "Population",
+    "World",
+    "PopulationStats",
+    "Stage1Config",
+    "Stage1Simulation",
+]
